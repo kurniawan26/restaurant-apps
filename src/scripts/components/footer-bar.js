1b291239d2d52685ff5c90914ee8dbf85bf2bce9
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2022-03-29 23:01:01 +0700

    Footer little edit style

--- src/scripts/components/footer-bar.js
+++ src/scripts/components/footer-bar.js
@@ -7,14 +7,10 @@ class Footbar extends HTMLElement {
   render() {
     this.innerHTML = `
     <footer class="footer">
-    <div class="container flex">
-      <div class="logo-col">
         <p class="copyright">
           Copyright &copy; <span class="year">2022</span> by Cofood, Inc. All
           rights reserved by Fauzi Kurniawan.
         </p>
-      </div>
-    </div>
   </footer>`;
   }
 }
